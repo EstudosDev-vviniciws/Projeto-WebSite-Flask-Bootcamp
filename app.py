74c22bdc111924958ed7ffa30de33e873218bef4
Vinícius Pinto Gonçalves <vinip8372@gmail.com>
2025-06-21 19:59:28 -0300

    Importa a função 'carrega_vagas_db' do arquivo 'database.py' e aplica nas rotas '/' e '/vagas' para carregar os dados dinamicamente do banco de dados

--- app.py
+++ app.py
@@ -1,51 +1,17 @@
 from flask import Flask, render_template, jsonify
+from database import carrega_vagas_db
 
 app = Flask(__name__)
 
-VAGAS = [
-    {
-        'id': 1,
-        'titulo': 'Analista de Dados',
-        'localidade': 'MG, Brasil',
-        'salario': 'R$ 4.700'    
-    },
-    
-    {
-        'id': 2,
-        'titulo': 'Engenheiro de Software',
-        'localidade': 'SP, Brasil',
-        'salario': 'R$ 3.700'
-    },
-    
-    {
-        'id': 3,
-        'titulo': 'Desenvolvedor Front-End',
-        'localidade': 'MG, Brasil',
-        'salario': 'R$ 3.000'
-    },
-    
-    {
-        'id': 4,
-        'titulo': 'Desenvolvedor Back-End',
-        'localidade': 'MG, Brasil',
-        'salario': 'R$ 4.200'
-    },
-    
-    {
-        'id': 5,
-        'titulo': 'Desenvolvedor Full-Stack',
-        'localidade': 'RJ, Brasil',
-        'salario': 'R$ 5.200'
-    }
-]
-
 @app.route('/')
 def hello():
-    return render_template("home.html", vagas=VAGAS)
+    vagas1 = carrega_vagas_db()
+    return render_template("home.html", vagas=vagas1)
 
 @app.route('/vagas')
 def lista_vagas():
-    return jsonify(VAGAS)
+    vagas1 = carrega_vagas_db()
+    return jsonify(vagas1)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
