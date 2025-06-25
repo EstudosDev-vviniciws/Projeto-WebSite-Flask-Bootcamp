db3762141f46370122e6ef063411ca2af77a24f4
Vinícius Pinto Gonçalves <vinip8372@gmail.com>
2025-06-24 22:02:21 -0300

    Renomeia a função 'hello' para 'home' e adiciona a rota '/vaga/<id>/inscricao' que processa os dados do formulário via POST e renderiza o template 'inscricao_concluida.html' com os dados da vaga e da inscrição

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, jsonify
-from database import carrega_vagas_db, carrega_vaga_db
+from flask import Flask, render_template, jsonify, request
+from database import carrega_vagas_db, carrega_vaga_db, adiciona_incricao
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def home():
     vagas1 = carrega_vagas_db()
     return render_template("home.html", vagas=vagas1)
 
@@ -19,6 +19,13 @@ def mostra_vaga(id):
         if not vaga1:
             return "Not Found", 404
         return render_template("detalhevaga.html", vaga=vaga1)
+    
+@app.route('/vaga/<id>/inscricao', methods=["GET", "POST"])
+def incricao_vaga(id):
+    vaga1 = carrega_vaga_db(id)
+    data = request.form
+    adiciona_incricao(id, data)
+    return render_template("inscricao_concluida.html", vaga=vaga1, inscricao=data)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
